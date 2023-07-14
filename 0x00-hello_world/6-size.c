419f07e8309d7ef2ead84c1e5a6806a11816e10d
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-07-13 17:21:54 -0700

    one

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,17 +3,18 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
+ * Description: A C program that prints the sizes of different data types
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("size of a char: %u byte(s)\n". sizeof(char));
-	printf("size of an int: %u byte(s)\n". sizeof(int));
-	printf("size of a long int: %u byte(s)\n". sizeof(long int));
-	printf("size of a long long int: %u byte(s)\n". sizeof(long long int));
-	printf("size of a float: %u byte(s)\n". sizeof(float));
-	return (0);
+    printf("size of a char: %zu byte(s)\n", sizeof(char));
+    printf("size of an int: %zu byte(s)\n", sizeof(int));
+    printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+    printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
+    printf("size of a float: %zu byte(s)\n", sizeof(float));
+
+    return (0);
 }
