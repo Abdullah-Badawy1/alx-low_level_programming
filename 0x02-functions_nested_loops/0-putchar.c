79c59976f50711af47f91f19669712bcaac0adf0
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-07-19 13:32:44 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,20 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main - Entry point
- *
- * Description: a program  with
- * areturn functionthat prints _putchar, followed by a new line.
- *
- * Return: 0 (success)
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-
-void areturn(void)
-{
-	char areturn2[] = "_putchar";
-
-	pritnf("%c\n", areturn2);
-}
-
 int main(void)
 {
-	areturn();
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
