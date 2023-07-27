873a59c341ec288d04aedcded98a5d2de89e2bb7
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-07-27 07:18:47 -0700

    a

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program.
- *
- * Description: The main function serves as the entry point of the program.
- * It initializes an array "a" of 5 integers, sets the value of the 3rd element
- * to 1024, and then attempts to access the 6th element of the integer pointer
- * "p" to store the value 98. Note that modifying the pointer "p" directly in
- * this manner leads to undefined behavior since it is not pointing to a valid
- * array of sufficient size.
- *
- * Return: Always 0 (indicating successful execution).
- */
 int main(void)
 {
 	int n;
@@ -20,17 +8,16 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-
 	/*
-	 * Write your line of code here...
-	 * Remember:
-	 * - You are not allowed to use the "a" array directly.
-	 * - You are not allowed to modify the "p" pointer.
-	 * - Use pointer arithmetic to access the required location.
-	 * - Only one statement is allowed.
-	 * - You are not allowed to code anything else than this line of code.
-	 */
+	* write your line of code here...
+	* Remember:
+	* - you are not allowed to use a
+	* - you are not allowed to modify p
+	* - only one statement
+	* - you are not allowed to code anything else than this line of code
+	*/
 	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
-}
+}}
