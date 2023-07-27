6aaf50784d24c00347c11fffc32ec70319b07653
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-07-27 06:33:55 -0700

    a

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encode a string using ROT13.
+ * @s: input string.
+ *
+ * Return: Pointer to the modified string.
+ */
+char *rot13(char *s)
+{
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = rot[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
