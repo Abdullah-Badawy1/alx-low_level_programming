ae45fc877bb9a5a8cee6da6265c4773caddc59ec
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-07-27 06:54:35 -0700

    a

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -4,12 +4,11 @@
  * main - Entry point of the program.
  *
  * Description: The main function serves as the entry point of the program.
- * It is automatically called when the program is executed. It may contain
- * the main logic of the program, including function calls, variable
- * declarations, and program flow control. The return value of the main
- * function is used as the exit status of the program, where a return value
- * of 0 typically indicates successful execution, and a non-zero value
- * indicates an error or abnormal termination.
+ * It initializes an array "a" of 5 integers, sets the value of the 3rd element
+ * to 1024, and then attempts to access the 6th element of the integer pointer
+ * "p" to store the value 98. Note that modifying the pointer "p" directly in
+ * this manner leads to undefined behavior since it is not pointing to a valid
+ * array of sufficient size.
  *
  * Return: Always 0 (indicating successful execution).
  */
@@ -31,7 +30,7 @@ int main(void)
 	 * - Only one statement is allowed.
 	 * - You are not allowed to code anything else than this line of code.
 	 */
-	*(p + 5) = 98;
+	p[5] = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
