155a3418338caad3424dfe31850b5914a60b28cc
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-08-02 03:25:05 -0700

    first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _puts_recursion - Prints a string using recursion, followed by a new line.
+ *
+ * This function takes a pointer to a string as input and recursively prints
+ * each character of the string until it reaches the null terminator ('\0').
+ *	When the end of the string is reached, a newline character is printed to
+ *	signify the end of the string.
+ *
+ * @s: Pointer to the input string to be printed.
+ */
+void _puts_recursion(char *s)
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+ the 8th project of c language 
