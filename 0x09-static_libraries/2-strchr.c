f2b00f3049ea8e4e205f319f9a72c7842dd20a54
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-08-04 11:14:05 -0700

    a

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,23 +1,38 @@
 #include "main.h"
 #include <stddef.h>
 /**
-*_strchr - Returns a pointer to the first occurrence
-*of the character c in the string s, or NULL if the
-*character is not found
-*
-*@s:string targeted
-*@c:character targeted
-*
-*Return: returns pointer to first occcurence of c
-*/
+ * _strchr - Returns a pointer to the first occurrence
+ *           of the character 'c' in the
+ *           string 's', or NULL if the
+ *           character is not found.
+ *
+ * Description: This function searches for
+ * the first occurrence of the character 'c'
+ *              in the string 's'. If found,
+ *              it returns a pointer to that location.
+ *              If the character is not found, it returns NULL.
+ *
+ * @s: Pointer to the string to be searched.
+ * @c: Character to search for.
+ *
+ * Return: Returns a pointer to the first
+ * occurrence of 'c' in 's', or NULL if not found.
+ */
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
-		;
+	while (s[i] != '\0' && s[i] != c)
+	{
+		i++;
+	}
+
 	if (s[i] == c)
+	{
 		return (s + i);
+	}
 	else
+	{
 		return (NULL);
+	}
 }
