dd1a0b9e89477cc50e36ff79cdf5d9b0f7f76102
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-08-10 03:49:16 -0700

    a

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * custom_realloc - Reallocates a memory block.
- * @ptr: Pointer to the memory previously allocated with malloc.
- * @old_size: Size of the memory block pointed to by ptr.
+ * _realloc - Reallocates a memory block.
+ * @ptr: Pointer to the memory previously allocated with a call to malloc.
+ * @old_size: Size of ptr.
  * @new_size: Size of the new memory to be allocated.
  *
  * Return: Pointer to the address of the new memory block.
  */
-void *custom_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *temp_block;
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (ptr == NULL)
 	{
@@ -30,8 +30,11 @@ void *custom_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		temp_block = malloc(new_size);
 		if (temp_block != NULL)
 		{
-			for (i = 0; i < (old_size < new_size ? old_size : new_size); i++)
+			while (i < old_size && i < new_size)
+			{
 				*((char *)temp_block + i) = *((char *)ptr + i);
+				i++;
+			}
 			free(ptr);
 			return (temp_block);
 		}
