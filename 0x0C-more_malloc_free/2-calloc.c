bfbbece3dc8a221101911e995c5b434d47be87c9
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-08-10 03:38:57 -0700

    a

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,42 +1,48 @@
 #include "main.h"
 
 /**
- * custom_string_nconcat - Concatenates two strings with a limit on bytes.
- * @str1: The first string.
- * @str2: The second string.
- * @limit: The maximum number of bytes from str2 to concatenate to str1.
+ * _calloc - Concatenates two strings with a limit on bytes.
+ * @nmemb: The first string.
+ * @size: The second string.
+ *
  *
  * Return: If memory allocation fails, returns NULL.
  *         Otherwise, returns a pointer to the concatenated memory.
  */
-char *custom_string_nconcat(char *str1, char *str2, unsigned int limit)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *concatenated;
-	unsigned int total_length = limit, i;
+	char *block;
+	unsigned int i;
 
-	if (str1 == NULL)
-		str1 = "";
-
-	if (str2 == NULL)
-		str2 = "";
-
-	for (i = 0; str1[i]; i++)
-		total_length++;
-
-	concatenated = malloc(sizeof(char) * (total_length + 1));
-
-	if (concatenated == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	total_length = 0;
+	block = malloc(nmemb * size);
 
-	for (i = 0; str1[i]; i++)
-		concatenated[total_length++] = str1[i];
+	if (block != NULL)
+	{
+		switch (size)
+		{
+			case 1:
+				for (i = 0; i < nmemb; i++)
+					block[i] = 0;
+				break;
 
-	for (i = 0; str2[i] && i < limit; i++)
-		concatenated[total_length++] = str2[i];
+			case 2:
+				for (i = 0; i < nmemb * 2; i++)
+					block[i] = 0;
+				break;
 
-	concatenated[total_length] = '\0';
+			default:
+				for (i = 0; i < nmemb * size; i++)
+					block[i] = 0;
+				break;
+		}
 
-	return (concatenated);
+		return (block);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
