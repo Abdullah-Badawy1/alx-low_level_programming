09d409b0637e1a3f094650f9ad21fdea4422059e
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-08-14 15:16:51 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - this descripction about which dog
+ * @name: dog's name
+ *
+ * @age: Dog's age
+ * @owner: Owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
