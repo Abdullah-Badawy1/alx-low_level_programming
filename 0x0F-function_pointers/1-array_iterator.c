57554156f3458019cb368a390ead9b3d6e042a09
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-08-16 02:07:58 -0700

    a

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,8 +11,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL)
 	return;
 
-	for (size_t i = 0; i < size; i++)
+	while (size-- > 0)
 	{
 		action(*array);
+		array++;
 	}
 }
