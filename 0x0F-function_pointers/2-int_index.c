41c0a230fec3e53417b1b6f7d172e69ce553deff
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-08-16 02:40:23 -0700

    a

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,10 +12,11 @@
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int index = 0;
 	if (array == NULL || cmp == NULL)
 		return (-1);
 
-	for (int index = 0; index < size; index++)
+	for ( ; index < size; index++)
 	{
 		if (cmp(array[index]) != 0)
 			return (index);
