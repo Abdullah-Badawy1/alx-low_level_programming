1f4c0f7e6177d27745dbb6a502d8c64d8fa02131
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-08-17 12:41:44 -0700

    a

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,15 +15,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int count;
 
 	va_start(valist, n);
-
 	count = 0;
 	while (count < n)
 	{
 		printf("%d", va_arg(valist, const unsigned int);
 		if (separator != NULL && count != (n - 1))
-		{
 			printf("%s", separator);
-		}
 		count++;
 	}
 	printf("\n");
