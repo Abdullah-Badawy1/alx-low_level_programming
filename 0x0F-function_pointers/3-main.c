b278dbb4a793a0fd9512363017190cbbc2293f76
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-08-18 11:12:02 -0700

    a

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,22 +11,31 @@
  */
 int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
-
+	int n1, n2;
+	char *s;
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	oprt = get_op_func(argv[2]);
+	n1 = atoi(argv[1]);
+	s = argv[2];
+	n2 = atoi(argv[3]);
 
-	if (!oprt)
+	if (get_op_func(s) == NULL || s[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	if ((*s == '/' && n2 == 0) || (*s == '%' && n2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(s)(n1, n2));
+
 	return (0);
 }
