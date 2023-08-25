d3ed8a7452c9ff4ec956e3f9f21b8991792845b9
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-08-24 19:06:39 -0700

    a

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,13 +10,13 @@ size_t list_len(const list_t *h)
 	const list_t *temp;
 
 
-	unsigned int counter = 0;
+	int counter = 0;
 
 
 
 	for (temp = h; temp; temp = temp->next)
 	{
-		counter = couter + 1;
+		counter = counter + 1;
 	}
 
 	return (counter);
