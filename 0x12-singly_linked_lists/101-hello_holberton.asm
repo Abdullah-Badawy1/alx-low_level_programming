e2c22e736272942dc5c360e3d10874f1b266d25e
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-08-24 19:38:58 -0700

    a

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,14 @@
-   global    main
-          extern    printf
+section .data
+    hello db 'Greetings from the Learning Realm!',0
+    format db '%s', 0
+
+section .text
+    global main
+    extern printf
+
 main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
-	  ret
-format: db `Hello, Holberton\n`,0
+    mov edi, hello        ; Load the address of the string to be printed
+    xor eax, eax          ; Clear eax register for return value
+    call printf           ; Call printf function
+    mov eax, 0            ; Set return value to 0
+    ret
