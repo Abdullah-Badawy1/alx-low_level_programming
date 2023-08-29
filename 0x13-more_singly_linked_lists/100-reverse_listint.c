5fcc6fc807e536c9b62ef044a287aca048ee01f4
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-08-29 13:15:21 -0700

    a

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -2,35 +2,30 @@
 #include "lists.h"
 
 /**
- * reverse_listint - Reverses the demand a listint_t linked list.
+ * reverse_listint - Reverses a listint_t linked list.
  * @head: Double pointer to the head of the listint_t linked list.
- * Return: A pointer to the first place node of the reversed list.
+ *
+ * Return: A pointer to the first node of the reversed list.
  */
-
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *next_node, *new_head;
+	listint_t *next_node, *prev_node;
 
-	switch (!(*head))
+	if (!(*head))
+		return (NULL);
+
+	prev_node = NULL;
+	next_node = (*head)->next;
+	(*head)->next = prev_node;
+	prev_node = *head;
+
+	for (; next_node;)
 	{
-		case 1:/* List is empty, return NULL */
-			return (NULL);
-		case 0:/* List is not empty */
-			new_head = NULL;
-			next_node = (*head)->next;
-			(*head)->next = new_head;
-			new_head = *head;
-
-			for (; next_node; *head = next_node)
-			{
-				next_node = (*head)->next;
-				(*head)->next = new_head;
-				new_head = *head;
-			}
-
-			return (*head);
+		*head = next_node;
+		next_node = (*head)->next;
+		(*head)->next = prev_node;
+		prev_node = *head;
 	}
 
-	return (NULL);
-	/* Return NULL if control reaches here */
+	return (*head);
 }
