05368d3f3cebac2dd0f0fdf200e8fbb8974ee3b7
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2023-08-31 12:16:13 -0700

    total file

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -8,18 +8,16 @@
 int get_endianness(void)
 {
 	unsigned int check = 1;
-	char *bytePointer;
+	char *bytePointer = (char *)&check;
 
-	bytePointer = (char *)&check;
-
-	/* Byte-order of multi-byte data types determines endianness */
-	switch (*bytePointer)
+	if (*bytePointer)
 	{
-		case 0:
-			/* Most significant byte (MSB) is 0, so little-endian */
-			return (1);
-		default:
-			/* MSB is 1, so big-endian */
-			return (0);
+		/* MSB is non-zero, so little-endian */
+		return (1);
+	}
+	else
+	{
+		/* MSB is zero, so big-endian */
+		return (0);
 	}
 }
