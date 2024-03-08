3f78127180b976d2f9ac23c3febab2e1b0cd754f
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2024-03-08 16:35:20 -0500

    Resolve1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,17 +8,12 @@
 *@n: Number of bytes to fill.
 *Return: Returns the pointer to the updated target memory area.
 */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
 	while (i < n)
 	{
-		if (i % 10)
-			printf(" ");
-		if (!(i % 10) && i)
-			printf("\n");
 		s[i] = b;
 		i++;
 		return (s);
