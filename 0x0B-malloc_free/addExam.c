09d0e58ae1f0c950e6437d33ab8ba04bd9df6d3c
Abdullah-Badawy1 <abdullah.rahim@btu.edu.eg>
2024-05-04 08:11:08 -0400

    Resolve Questions

--- 0x0B-malloc_free/addExam.c
+++ 0x0B-malloc_free/addExam.c
@@ -45,3 +45,47 @@ int main(int argc, char **argv)
     return (0);
 }
 
+/* ChatGpt suggestion */ 
+#include <stdio.h>
+#include <stdlib.h>
+
+int *array(int size) {
+    if (size <= 0)
+        return NULL;
+    return malloc(sizeof(int) * size);
+}
+
+int main() {
+    int size;
+    int *ptr1;
+
+    printf("Enter the size of the array: ");
+    if (scanf("%d", &size) != 1 || size <= 0) {
+        printf("Invalid input or size\n");
+        return 1;
+    }
+
+    ptr1 = array(size);
+    if (ptr1 == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    for (int i = 0; i < size; i++) {
+        printf("Array[%d]: ", i);
+        if (scanf("%d", &ptr1[i]) != 1) {
+            printf("Failed to read integer\n");
+            free(ptr1);
+            return 1;
+        }
+    }
+
+    printf("You have an array[%d]\n", size);
+    for (int j = 0; j < size; j++) {
+        printf("%d ", ptr1[j]);
+    }
+    printf("\n");
+
+    free(ptr1);
+    return 0;
+}
